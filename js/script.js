327b3a078b40966619a7da247e284b915e99bf30
Vadim Tsoy <squintjester@gmail.com>
2023-05-11 22:43:18 +0600

    Added offers section and footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-const swiper = new Swiper(".swiper", {
+const swiperCoach = new Swiper(".swiper__coach", {
   slidesPerView: 6,
+  loop: false,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+const swiperSingle = new Swiper(".swiper__single", {
+  slidesPerView: 1,
   loop: false,
 
   navigation: {
